02236d95d8ae8858081cf3ebb86ab3451fb2f032
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-09-27 16:02:43 +0200

    feat(movie-card): add movie card component with skeleton loader feat(actor-card): add actor card component with support for full and compact views

---
+++ src/components/cards/ActorCard.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {Skeleton} from "@/components/ui/skeleton/index.js";
+
+let props = defineProps({
+  title: {
+    type: String,
+    required: false
+  },
+  format: {
+    type: String,
+    required: false,
+    default: 'full',
+    validator: (value) => ['compact', 'full'].includes(value)
+  }
+})
+</script>
+
+<template>
+  <router-link to="/actors/leonardo-di-caprio" v-if="title && format === 'full'">
+    <div class="flex flex-col gap-4 items-center">
+      <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8yhDDv3b5ixR0-eeKOz9sfth6qlJxUFRuMk3fRJktRUVlO4fJ"
+          class="w-60 h-60 rounded-full object-cover" alt="">
+      <div class="flex flex-col gap-0.5 self-start"><h3 class="text-lg font-semibold">Leonardo Di Caprio</h3>
+        <p class="text-sm text-gray-400">Ukrainian</p></div>
+    </div>
+  </router-link>
+  <div v-else-if="!title && format === 'full'">
+    <div class="flex flex-col gap-4 items-center">
+<Skeleton class="w-60 h-60 rounded-full"></Skeleton>
+      <div class="flex flex-col w-full gap-2">
+        <Skeleton class="h-6 w-2/3"></Skeleton>
+        <Skeleton class="h-3 w-1/3"></Skeleton>
+      </div>
+
+    </div>
+    </div>
+
+  <router-link to="/actors/leonardo-di-caprio" v-if="title && format === 'compact'" class="w-36 h-52 bg-amber-100 rounded-xl">
+    {{title}}
+  </router-link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/cards/MovieCards.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { Skeleton } from '@/components/ui/skeleton'
+
+let props = defineProps({
+  title: {
+    type: String,
+    required: false
+  },
+  year: {
+    type: Number,
+    required: false
+  },
+  coverURL: {
+    type: String,
+    required: false,
+  },
+  to: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <router-link :to="'/movie/'+to" class="" v-if="title">
+    <div class="duration-300 ring-1 ring-white/5 ring-inset hover:drop-shadow-lg hover:scale-105">
+      <div class="min-h-48 rounded-xl mb-2" :style="{ background: 'url(' + coverURL + ')', backgroundSize: 'cover', backgroundPosition: 'center' }">
+
+      </div>
+
+      <h2 class="text-lg font-bold">{{title}}</h2>
+      <p class="text-sm text-gray-500">{{ year }}</p></div>
+  </router-link>
+
+  <div v-else>
+    <div class="ring-1 ring-white/5 ring-inset">
+      <Skeleton class="min-h-48 rounded-xl mb-2">
+      </Skeleton>
+
+      <div class="flex flex-col gap-2">
+        <Skeleton class="h-7 w-2/3"></Skeleton>
+        <Skeleton class="h-4 w-1/3"></Skeleton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
