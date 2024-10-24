f2a78a8d770abbf45f1b9b9567337c277dc8cc27
D3ller <corentin.nelhomme@gmail.com>
2024-10-24 18:33:34 +0200

    feat. adding auto fetch actors from the api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,35 +2,51 @@
 import MovieCards from "@/components/cards/MovieCards.vue";
 import {Button} from "@/components/ui/button/index.js";
 import ActorCard from "@/components/cards/ActorCard.vue";
+import axios from "axios";
+import {inject, ref} from "vue";
 
-let films = [
-  {title: 'Titanic', coverURL: 'https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2024/03/shutterstock_1837038775-1.jpg', year: 2023, slug: 'titanic'},
-  {title: 'Inception', coverURL: 'https://www.screentune.com/wp-content/uploads/2023/10/Critique-Inception-2010-ScreenTune.jpg', year: 2008, slug: 'inception'},
-  {title: 'Django Unchained', coverURL: 'https://media.gqmagazine.fr/photos/5dea6130061f7b00082f3405/16:9/w_2560%2Cc_limit/Djangounchained.jpg', year: 2012, slug: 'django-inception'},
-  {title: 'The Revenant', coverURL: 'https://m.media-amazon.com/images/S/pv-target-images/7f49467e54e5639a175e4f0f3bffa565be6542aa08af7609da8eb351b1982d81._SX1080_FMjpg_.jpg', year: 2015, slug: 'the-revenant'},
-]
+const api_url = inject('api_url')
+let films = ref(null)
+let actors = ref(null)
+
+let getMoviesAndActors = () => {
+  axios.get(`${api_url}/movies?itemsPerPage=4`).then((e) => {
+    films.value = e.data.member
+  })
+
+  axios.get(`${api_url}/actors?itemsPerPage=4`).then((e) => {
+    actors.value = e.data.member
+    console.log(actors.value)
+  })
+}
+
+getMoviesAndActors()
 </script>
 
 <template>
   <div class="flex justify-between items-center"><h1 class="text-2xl font-semibold">Film récents</h1>
 
-    <Button variant="secondary" as-child><router-link to="/movies">Voir plus</router-link></Button>
+    <Button as-child variant="secondary">
+      <router-link to="/movies">Voir plus</router-link>
+    </Button>
   </div>
 
-  <div class="grid grid-cols-12 space-x-3 mt-4">
-    <div class="col-span-3" v-for="(i, index) in films">
-      <MovieCards :key="i" :to="i.slug" :cover-u-r-l="i.coverURL" :title="i.title" :year="i.year"></MovieCards>
+  <div class="grid grid-cols-12 gap-6 mt-5">
+    <div v-for="(i, index) in films" :key="index" class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+      <MovieCards :key="i" :cover-u-r-l="i.media" :title="i.title" :to="i.id" :year="i.releaseDate"></MovieCards>
     </div>
   </div>
 
   <div class="flex justify-between items-center mt-20"><h1 class="text-2xl font-semibold">Acteurs ajoutés</h1>
 
-    <Button variant="secondary" as-child><router-link to="/actors">Voir plus</router-link></Button>
+    <Button as-child variant="secondary">
+      <router-link to="/actors">Voir plus</router-link>
+    </Button>
   </div>
 
-  <div class="grid grid-cols-12 mt-4">
-    <div class="col-span-3" v-for="(i, index) in 4">
-      <ActorCard :key="i"></ActorCard>
+  <div class="grid grid-cols-12 mt-5 gap-6">
+    <div v-for="(i, index) in actors" :key="index" class="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3">
+      <ActorCard :key="i" :title="i.firstname + ' ' + i.lastname" :to="i.id"></ActorCard>
     </div>
   </div>
 
